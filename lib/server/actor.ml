c4a29e0493d878a54f0a595e919da1ecd3f35178
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-03-06 10:48:14 +0000

    added signature verification

--- lib/server/actor.ml
+++ lib/server/actor.ml
@@ -1,3 +1,4 @@
+open Containers
 
 let with_user req then_ =
   let (let+) x f = Common.request_bind req x f in
@@ -9,6 +10,7 @@ let with_user req then_ =
 
 let (let+) x f = x f
 let (let*) x f = Lwt.bind x f
+let (let*!) x f = Lwt_result.bind x f
 
 
 
@@ -30,9 +32,47 @@ let handle_inbox_get req =
   Dream.log "GET to %s/inbox" (Dream.param req "username");
   Dream.respond ~status:`OK ""
 
+let string_safe line elt kont =
+  match elt with
+  | `String str -> kont str
+  | _ ->
+    Lwt.return (Error (Int.to_string line ^ "could not convert " ^ Yojson.Safe.to_string elt ^ " into a string"))
+
+let member_safe line name elt kont =
+  match elt with
+  | `Assoc ls -> begin match List.assoc_opt ~eq:String.(=) name ls with
+    | Some res -> kont res
+    | None ->
+      Lwt.return (Error (Int.to_string line ^ "could not deref " ^ name ^ " from: " ^ Yojson.Safe.to_string elt))      
+    end
+  | _ -> Lwt.return (Error (Int.to_string line ^ "could not deref " ^ name ^ " from: " ^ Yojson.Safe.to_string elt))
+
+let lookup_request url : (X509.Public_key.t, 'a) result Lwt.t =
+  let*! (_resp, body) =
+    try
+      let* pair =
+        Cohttp_lwt_unix.Client.get
+          ~headers:(Cohttp.Header.of_list ["Content-Type", Activitypub.ContentType.activity_json])
+          (Uri.of_string url) in
+      Lwt_result.return pair
+    with exn -> Lwt.return (Result.of_exn exn) in
+  let* body = Cohttp_lwt.Body.to_string body in
+  let*! body_json = Lwt.return @@ try Ok (Yojson.Safe.from_string body) with exn -> Result.of_exn exn in
+  let+ public_key = member_safe __LINE__ "publicKey" body_json in
+  let+ public_key_pem = member_safe __LINE__ "publicKeyPem" public_key in
+  let+ public_key_pem = string_safe __LINE__ public_key_pem in
+  let pub_key = X509.Public_key.decode_pem (Cstruct.of_string public_key_pem)
+              |> Result.map_err (fun (`Msg err) -> err) in
+  Lwt.return pub_key
+  
 
 let handle_inbox_post req =
   Dream.log "POST to %s/inbox" (Dream.param req "username");
+  let* verification = Auth.verify_request lookup_request req in
+  begin match verification with
+  | Ok status -> Dream.log "verification status was %b" status
+  | Error e -> Dream.log "error while verifying: %s" e
+  end;
   let* body = Dream.body req in
   Dream.log "DATA: %s" body;
   Dream.respond ~status:`OK ""
@@ -47,6 +87,7 @@ let handle_outbox_post req =
   let* body = Dream.body req in
   Dream.log "DATA: %s" body;
   Dream.respond ~status:`OK ""
+
 
 
 let route config = 
