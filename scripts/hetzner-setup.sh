83accd6281d8ba6f46169cb481c9fdc6ad8e3206
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-03-21 03:35:11 +0000

    updated

--- scripts/hetzner-setup.sh
+++ scripts/hetzner-setup.sh
@@ -4,10 +4,10 @@
 sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
 
 # 2. install general deps
-sudo apt install libc-dev git make gcc argon2 unzip bubblewrap sqlite3 libev-dev libffi-dev libgmp-dev libsqlite3-dev libssl-dev pkg-config
+sudo apt install libc-dev git make gcc argon2 unzip bubblewrap sqlite3 libev-dev libffi-dev libgmp-dev libsqlite3-dev libssl-dev pkg-config 
 
 # 3. install sass
-curl -L https://github.com/sass/dart-sass/releases/download/1.49.9/dart-sass-1.49.9-linux-x64.tar.gz /tmp/dart-sass.tar.gz
+curl -L https://github.com/sass/dart-sass/releases/download/1.49.9/dart-sass-1.49.9-linux-x64.tar.gz -o /tmp/dart-sass.tar.gz
 (cd /tmp/ && tar xzf ./dart-sass.tar.gz && sudo cp ./dart-sass/sass /usr/local/bin/)
 
 # 4. initialise opam
@@ -21,7 +21,7 @@ opam install dune \
      dream jwto\
      caqti caqti-driver-sqlite3\
      alcotest alcotest-lwt \
-     yojson ppx_yojson_conv\
+     yojson decoders decoders-yojson \
      ppx_deriving\
      argon2 sqlite3 x509 ptime bos tyxml \
      cohttp-lwt-unix \
