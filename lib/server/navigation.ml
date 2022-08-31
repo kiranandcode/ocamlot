bc86c7b95caa59583257e4fb549834a02ac69c40
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-08-31 13:49:58 +0100

    minor changes

--- lib/server/navigation.ml
+++ lib/server/navigation.ml
@@ -6,5 +6,5 @@ let build_navigation_bar req =
     match user with
     | None -> [ "Log in", "/login", false; "Register", "/register", false ]
     | Some _ -> [ "Profile", "/profile", false; "Log out", "/logout", true] in
-  let navigation_components = ["feed", "/feed", false] in
+  let navigation_components = ["Feed", "/feed", false] in
   return_ok (navigation_components @ login)
